9bd57974dc7fbe2e7a83e291ca7b7cd4a2340e9a
Joshua Hoblitt <josh@hoblitt.com>
2014-12-18 12:00:13 -0700

    disable leaflet world "wrapping"

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ var map = L.map('map').setView([1.0, 1.0], 0);
 
 L.tileLayer('./img/{z}/foo_{x}_{y}.jpg', {
     maxZoom: 6,
-    id: 'epo-test1'
+    id: 'epo-test1',
+    noWrap: true
 }).addTo(map);
